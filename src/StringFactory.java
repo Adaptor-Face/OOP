ae0e4787ed4b6e2a43226c6e3dbef463a2513f52
Kristoffer Rogne <Kristoffer_rogne@hotmail.com>
2016-04-22 11:08:27 +0200

    Made StringFactory be used over LiteratureView

--- src/StringFactory.java
+++ src/StringFactory.java
@@ -12,6 +12,11 @@ public class StringFactory {
      */
     public StringFactory() {
     }
+    /**
+     * Creates the info string for any literature.
+     * @param literature The literature to create an info string for.
+     * @return the info string for any literature.
+     */
     public String getInfo(Literature literature) {
         String stringToReturn = "Title: " + literature.getTitle()
                 + getInfoFromLiterature(literature)
@@ -19,6 +24,11 @@ public class StringFactory {
         return stringToReturn;
     }
     
+    /**
+     * Creates a string of all details any literature has.
+     * @param literature The literature to create a detail string for.
+     * @return a string of all details any literature has.
+     */
     public String getDetails(Literature literature) {
         String stringToReturn = "Title: " + literature.getTitle();
         stringToReturn += getLiteratureDetails(literature);
@@ -28,6 +38,12 @@ public class StringFactory {
         return stringToReturn;
     }
 
+    /**
+     * Creates a string of only necessary information any literature may hold.
+     * Returns an empty string if the literature has nothing important to show.
+     * @param literature The literature to get the info from.
+     * @return a string of only necessary information any literature may hold.
+     */
     private String getInfoFromLiterature(Literature literature) {
         if (literature instanceof Book) {
             Book book = (Book) literature;
@@ -43,6 +59,13 @@ public class StringFactory {
         }
         return "";
     }
+    
+    /**
+     * Creates a string of all information any literature may hold.
+     * Returns an empty string if the literature has nothing important to show.
+     * @param literature The literature to get the details from.
+     * @return a string of all information any literature may hold.
+     */
     private String getLiteratureDetails(Literature literature) {
         String returnString = "";
         if (literature instanceof Book) {
